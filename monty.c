c3211374057b363ef49bbe76a9cad2ace6efc354
Thandeka325 <marhwayiza325@gmail.com>
2024-09-26 18:05:44 -0700

    Project 0x19. C - Stacks, Queues - LIFO, FIFO

--- monty.c
+++ monty.c
@@ -39,8 +39,8 @@ int main(int argc, char *argv[])
 		bus.content = content;
 		counter++;
 		if (read_line > 0)
-		
 			execute(content, &stack, counter, file);
+
 		free(content);
 	}
 	free_stack(stack);
