75dc8ba79647cb5f547e49ff26a1be5404955413
Thandeka325 <marhwayiza325@gmail.com>
2024-09-26 17:16:28 -0700

    Project 0x19. C - Stacks, Queues - LIFO, FIFO

--- monty.c
+++ monty.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
 	if (!file)
 	{
-		fprintf(stderr, "Error: Cant't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (read_line > 0)
